858172108ea93267c124e06a2dac70ea53123189
Lilach E <lilachelayza@gmail.com>
2024-05-15 10:25:39 +0300

    delete files

--- app/src/World.tsx
+++
@@ -1,7 +0,0 @@
-// class World {
-//     constructor(config) {
-//         this.element = config.element;
-//         this.canvas = this.element.querySelector(".gameCanvas");
-//         this.ctx = this.canvas.getContext("2d");
-//     }
-// }
